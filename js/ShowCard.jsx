d3e562833cbac8f83b0b9b2a3d2267937b1f4f52
KEITH L ROBINSON <krobnson@Keiths-MBP.local>
2017-12-18 10:39:36 -0600

    commit before transition

---
+++ js/ShowCard.jsx
@@ -0,0 +1,43 @@
+// @flow
+
+import React, {Component} from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
+const Wrapper = styled((Link: any))`
+  width: 32%;
+  border: 2px solid #333;
+  border-radius: 4px;
+  margin-bottom: 25px;
+  padding-right: 10px;
+  overflow: hidden;
+  color: black;
+  text-decoration: none;
+`;
+
+const Image = styled.img`
+  width: 46%;
+  float: left;
+  margin-right: 10px;
+`;
+
+class ShowCard extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+  props: Show;
+  render() {
+    return (
+      <Wrapper to={`/details/${this.props.imdbID}`}>
+        <Image alt={`${this.props.title} Show Poster`} src={`/public/img/posters/${this.props.poster}`} />
+        <div>
+          <h3>{this.props.title}</h3>
+          <h4>({this.props.year})</h4>
+          <p>{this.props.description}</p>
+        </div>
+      </Wrapper>
+    );
+  }
+}
+
+export default ShowCard;
